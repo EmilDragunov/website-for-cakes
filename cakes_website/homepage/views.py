f7995d4295e1216b1a64102babc44dd786644023
Emil Dragunov <nowruzov.e@yandex.ru>
2024-07-04 15:48:00 +0300

    up

--- cakes_website/homepage/views.py
+++ cakes_website/homepage/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from cake.models import Cake
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def index(request):
@@ -8,8 +9,18 @@ def index(request):
         is_published=True,
         is_on_main=True,
         category__is_published=True
-    )
+    ).order_by('title')
+    paginator = Paginator(cake_list, 1)
+    page_number = request.GET.get('page')
+    try:
+        cakes = paginator.page(page_number)
+    except PageNotAnInteger:
+        cakes = paginator.page(1)
+    except EmptyPage:
+        cakes = paginator.page(paginator.num_pages)
+
     context = {
-        'cake_list': cake_list,
+        'cake_list': cakes,
+        'paginator': paginator
     }
     return render(request, template, context)
